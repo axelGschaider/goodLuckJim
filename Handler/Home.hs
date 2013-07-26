74a0fe2119eced20397ea37faaae1cc448c96831
Axel Gschaider <axel.gschaider@gmail.com>
2013-07-26 21:34:32 +0200

    finaly some working hash

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -2,14 +2,13 @@
 module Handler.Home where
 
 import Import
+import Data.Time
 
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
+import Data.ByteString.Char8 (pack)
+import Data.Char (isAlphaNum)
+import Crypto.Hash.SHA256 (hash)
+
+
 getHomeR :: Handler Html
 getHomeR = do
     (formWidget, formEnctype) <- generateFormPost secretForm
@@ -17,36 +16,32 @@ getHomeR = do
         aDomId <- newIdent
         setTitle "Ei äm se pätsch häda!"
         $(widgetFile "homepage")
-    --(formWidget, formEnctype) <- generateFormPost sampleForm
-    --let submission = Nothing :: Maybe (FileInfo, Text)
-    --    handlerName = "getHomeR" :: Text
-    --defaultLayout $ do
-    --    aDomId <- newIdent
-    --    setTitle "Welcome To Yesod!"
-    --    $(widgetFile "homepage")
+
 
 postHomeR :: Handler Html
 postHomeR = do
     ((result, formWidget), formEnctype) <- runFormPost secretForm
     defaultLayout $ do
         aDomId <- newIdent
-        setTitle "changed"
+        mKey <- liftIO getMessageKey
+        setTitle $ toHtml mKey
         $(widgetFile "homepage")
-    --((result, formWidget), formEnctype) <- runFormPost sampleForm
-    --let handlerName = "postHomeR" :: Text
-    --    submission = case result of
-    --        FormSuccess res -> Just res
-    --        _ -> Nothing
 
-    --defaultLayout $ do
-    --    aDomId <- newIdent
-    --    setTitle "Welcome To Yesod!"
-    --    $(widgetFile "homepage")
+preSalt :: String
+preSalt = "ASDdasdj2 1sdf2"
+
+postSalt :: String
+postSalt = "fskjs23823e d2jh2d 2djhbsd**DSF§ſðđªð"
+
+hashIt :: String -> String
+hashIt =  show . hash . pack . (++ postSalt) . ( preSalt ++ )
+
+createMessageKey :: String -> String
+createMessageKey = filter ( isAlphaNum ) . hashIt
+
+getMessageKey :: IO String 
+getMessageKey = fmap ( createMessageKey . show ) getCurrentTime
 
 secretForm :: Form Text
 secretForm = renderDivs $ areq textField "The Message" Nothing
 
---sampleForm :: Form (FileInfo, Text)
---sampleForm = renderDivs $ (,)
---    <$> fileAFormReq "Choose a file"
---    <*> areq textField "What's on the file?" Nothing
